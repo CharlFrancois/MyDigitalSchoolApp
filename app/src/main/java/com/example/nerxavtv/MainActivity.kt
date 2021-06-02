ee3101224ef2663fa83f9298e48686cb48d55c96
Charles François <charfrancois@auchan.fr>
2021-06-02 14:12:58 +0200

    :construction: Add bdd to project

--- app/src/main/java/com/example/nerxavtv/MainActivity.kt
+++ app/src/main/java/com/example/nerxavtv/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.nerxavtv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nerxavtv.data.Training
+import com.example.nerxavtv.data.TrainingDao
+import com.example.nerxavtv.data.TrainingDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_training)
     }
 }
--- app/src/main/java/com/example/nerxavtv/data/TrainingDao.kt
+++ app/src/main/java/com/example/nerxavtv/data/TrainingDao.kt
@@ -1,10 +1,14 @@
 package com.example.nerxavtv.data
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 
     @Dao
     interface TrainingDao {
         @Query("SELECT * FROM training")
         fun getAll(): List<Training>
+
+        @Insert
+        fun insert(vararg training: Training)
     }
