1701062ab37ca3412074b5bf7e52350ac5eb49e7
Antoine CHAMBRIN <antoinechambrin@gmail.com>
2021-06-15 18:52:32 +0200

    bar de navigation fonctionnel + carte + btn : PB YTB PLAYER

--- app/src/main/java/com/example/nerxavtv/MainActivity.kt
+++ app/src/main/java/com/example/nerxavtv/MainActivity.kt
@@ -3,84 +3,30 @@ import android.app.SearchManager
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mapFragment: SupportMapFragment
-    lateinit var googleMap: GoogleMap
-
-  val api_key = "AIzaSyCnrcRd-zMj5Sq9twBs3V9mFskq65nyRt4"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_contact_page)
+        setContentView(R.layout.activity_main);
 
-        mapFragment = supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment
-        mapFragment.getMapAsync(OnMapReadyCallback {
-            googleMap = it
+        val navHostFragment = supportFragmentManager?.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = navHostFragment.navController
 
-            val localisation1 = LatLng(50.63331985473633,3.0680081844329834)
-            googleMap.addMarker(MarkerOptions().position(localisation1).title("MyDigitalSchool"))
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(localisation1, 15f))
-            googleMap.setOnInfoWindowClickListener {
-                MaRedirect()
-            }
-        })
+        bottomNavigationView.setupWithNavController(navController)
     }
-
-    fun call(view: View) {
-        val dialIntent = Intent(Intent.ACTION_DIAL)
-        dialIntent.data = Uri.parse("tel:" + "+33366060696")
-        startActivity(dialIntent)
-    }
-
-    fun sendEmail(view: View){
-        val intent = Intent(Intent.ACTION_SENDTO)
-        intent.data = Uri.parse("mailto: lille@mydigitalschool.com")
-        startActivity(intent)
-    }
-
-    fun MaRedirect() {
-        val intent = Intent(Intent.ACTION_WEB_SEARCH)
-        intent.putExtra(SearchManager.QUERY, "https://www.mydigitalschool.com/ecole-multimedia-lille")
-        startActivity(intent)
-    }
-
-    // Get reference to the view of Video player
-  //  val ytPlayer = findViewById<YouTubePlayerView>(R.id.ytPlayer)
-
-   // ytPlayer.initialize(api_key, object : YouTubePlayer.OnInitializedListener{
-      // Implement two methods by clicking on red error bulb
-      // inside onInitializationSuccess method
-      // add the video link or the
-      // playlist link that you want to play
-      // In here we also handle the play and pause functionality
-    //  override fun onInitializationSuccess(
-      //  provider: YouTubePlayer.Provider?,
-       // player: YouTubePlayer?,
-       // p2: Boolean
-      //) {
-       // player?.loadVideo("ZDFietBFL6s")
-        //player?.play()
-      //}
-
-      // Inside onInitializationFailure
-      // implement the failure functionality
-      // Here we will show toast
-      //override fun onInitializationFailure(
-        //p0: YouTubePlayer.Provider?,
-        //p1: YouTubeInitializationResult?
-      //) {
-        //Toast.makeText(this@MainActivity , "Video player Failed" , Toast.LENGTH_SHORT).show()
-      //}
-  //  }//)
-  //}
 }
--- app/src/main/java/com/example/nerxavtv/contactPage.kt
+++ app/src/main/java/com/example/nerxavtv/contactPage.kt
@@ -1,5 +1,6 @@
 package com.example.nerxavtv
 
+import android.app.SearchManager
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -9,6 +10,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.findFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,6 +27,9 @@ class contactPage : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    lateinit var mapFragment: SupportMapFragment
+    lateinit var googleMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,5 +45,45 @@ class contactPage : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contact_page, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+            mapFragment = childFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment
+            mapFragment.getMapAsync(OnMapReadyCallback {
+                googleMap = it
+
+                val localisation1 = LatLng(50.63331985473633,3.0680081844329834)
+                googleMap.addMarker(MarkerOptions().position(localisation1).title("MyDigitalSchool"))
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(localisation1, 15f))
+                googleMap.setOnInfoWindowClickListener {
+                    MaRedirect()
+                }
+            })
+        view.findViewById<Button>(R.id.callBtn).setOnClickListener{
+            call(it)
+        }
+        view.findViewById<Button>(R.id.button).setOnClickListener{
+            sendEmail(it)
+        }
+    }
+
+    fun call(view: View) {
+        val dialIntent = Intent(Intent.ACTION_DIAL)
+        dialIntent.data = Uri.parse("tel:" + "+33366060696")
+        startActivity(dialIntent)
+    }
+
+    fun sendEmail(view: View){
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto: lille@mydigitalschool.com")
+        startActivity(intent)
+    }
+
+    fun MaRedirect() {
+        val intent = Intent(Intent.ACTION_WEB_SEARCH)
+        intent.putExtra(SearchManager.QUERY, "https://www.mydigitalschool.com/ecole-multimedia-lille")
+        startActivity(intent)
     }
 }
