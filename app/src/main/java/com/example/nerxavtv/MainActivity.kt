ee278a1524de31e890a7386db094ccae147172fc
Charles François <charfrancois@auchan.fr>
2021-06-15 17:52:17 +0200

    :sparkles: add get element by id

--- app/src/main/java/com/example/nerxavtv/MainActivity.kt
+++ app/src/main/java/com/example/nerxavtv/MainActivity.kt
@@ -2,13 +2,40 @@ package com.example.nerxavtv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import com.example.nerxavtv.data.Training
 import com.example.nerxavtv.data.TrainingDao
 import com.example.nerxavtv.data.TrainingDatabase
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    val viewModel: TrainingViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_training)
+
+        TrainingDatabase.getInstance(this)
+        val trainingTitle: TextView = findViewById(R.id.textView2)
+        val trainingTitleObserver = Observer<Training?>{
+            trainingTitle.text = it?.title
+        }
+
+        val trainingDescription: TextView = findViewById(R.id.textView4)
+        val trainingDescriptionObserver = Observer<Training?>{
+            trainingDescription.text = it?.description
+        }
+
+        val trainingCode: TextView = findViewById(R.id.textView5)
+        val trainingCodeObserver = Observer<Training?>{
+            trainingCode.text = it?.code
+        }
+
+        viewModel.training.observe(this, trainingTitleObserver)
+        viewModel.training.observe(this, trainingDescriptionObserver)
+        viewModel.training.observe(this, trainingCodeObserver)
+        viewModel.findTrainingById(1)
     }
 }
---
+++ app/src/main/java/com/example/nerxavtv/TrainingViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.nerxavtv
+
+import android.app.Application
+import android.content.Context
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.nerxavtv.data.Training
+import com.example.nerxavtv.data.TrainingDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Exception
+
+class TrainingViewModel : ViewModel() {
+    val training: MutableLiveData<Training?> = MutableLiveData<Training?>(null)
+    fun findTrainingById(i: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val t = trainingDao?.getOneById(i)
+            withContext(Dispatchers.Main) {
+                training.value = t
+            }
+        }
+    }
+
+    private val trainingDao = TrainingDatabase.INSTANCE?.trainingDao()
+
+    private val training1 = com.example.nerxavtv.data.Training(
+        id = 1,
+        title = "Bachelor E-Business",
+        description = "certification professionnelle reconnue par l'État de niveau 6 : \"Concepteur Réalisateur Web et Digital\". Arrêté du 11 juillet 2018 publié au JO du 21 juillet 2018. Efficom.",
+        code = "RNCP29826"
+    )
+    private val training2 = com.example.nerxavtv.data.Training(
+        id = 2,
+        title = "Bachelor Développeur Web",
+        description = "certification professionnelle reconnue par l'État de niveau 6 : \"Concepteur - Développeur d'applications\" Arrêté du 12 octobre 2007, JO modificatif du 17 avril 2018. Ministère Chargé de l'Emploi. ",
+        code = "RNCP31678"
+    )
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                trainingDao?.insert(training1, training2)
+            }
+            catch (e: Exception) {}
+        }
+    }
+
+    var title : String? = null
+    var description : String? = null
+    var code : String? = null
+}
