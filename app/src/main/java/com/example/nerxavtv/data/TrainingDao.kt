a44f5cf043a4b5cfc4487fa4f1778d848560f112
Charles François <charfrancois@auchan.fr>
2021-06-17 11:53:25 +0200

    :sparkles: comment database

--- app/src/main/java/com/example/nerxavtv/data/TrainingDao.kt
+++ app/src/main/java/com/example/nerxavtv/data/TrainingDao.kt
@@ -6,12 +6,15 @@ import androidx.room.Query
 
     @Dao
     interface TrainingDao {
+        // Récupére l'ensemble de la collection "training"
         @Query("SELECT * FROM training")
         fun getAll(): List<Training>
 
+        // Récupére un élement de la collection "training" par rapport à l'id qu'on lui passe en paramètre
         @Query("SELECT * FROM training WHERE id = :id")
         fun getOneById(id: Int): Training
 
+        // Ajoute un élément à la collection "training'
         @Insert
         fun insert(vararg training: Training)
     }
